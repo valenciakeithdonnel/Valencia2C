dc0764247c5c6969df30c73bfa8d9a7d3e0e1cce
valenciakeithdonnel <valencia.kd.bsinfotech@gmail.com>
2024-07-10 08:25:22 +0800

    Working Login and Logout with Display Data

--- dcrm/views.py
+++ dcrm/views.py
@@ -1,14 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .models import Records
 
 # Create your views here.
 
 def home(request):
     #DO SOMETHING
 
-    return render(request, 'home.html')
+    records = Records.objects.all()
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in!")
+            return redirect('home')
+        else:
+            messages.success(request, "Error! Try again")  
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {'records':records})
+
 
 
 
 def register(request):
 
     return render(request, 'register.html')
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out!")  
+    return redirect('home')
